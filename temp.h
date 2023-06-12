809ca9b3094ff8c722d9104c71b6ba6c64fb8a80
Josh H <jhur2002@gmail.com>
2023-06-12 11:45:59 -0400

    Correctly extracts dataset into 2D matrix

---
+++ temp.h
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<vector<string>> read_CSV(const string& filename);
